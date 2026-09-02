6ff7839a18921d749e7a097469b75bd7c5c611e7
BaldwinZhao <lazytortoisezzzz@gmail.com>
2026-09-02 18:08:44 +0800

    [learn][A0S2] cuda c++ smoke test

---
+++ docs/envrionment.md
@@ -0,0 +1,8 @@
+
+WSL Distribution and version：Ubuntu 2(Ubuntu 26.04 LTS)
+GPU型号与现存：NNVIDIA GeForce RTX 4060 Ti；8188 MiB
+Windos NVIDIA Driver Version:590.26
+CUDA Compatibility Version: 12.9 ?
+nvcc 显示的 CUDA Toolkit Version: V13.3.73
+cmake version: 4.2.3
+ninja version: 1.13.2
---
+++ src/smoke_test.cu
@@ -0,0 +1,56 @@
+
+#include <cuda_runtime.h>
+#include <cstdlib>
+#include <iostream>
+
+#define VALUE 43
+
+__global__ void write_value(int* output) {
+	output[0] = VALUE;
+}
+
+__global__ void write_value(int* output, int value) {
+	output[0] = value;
+}
+
+int main() {
+
+	int* device_output = nullptr;
+	int host_output = 0;
+
+	cudaError_t status = cudaMalloc(&device_output, sizeof(device_output));
+	if (status != cudaSuccess) {
+		std::cerr << "cudaMalloc Failed: " << cudaGetErrorString(status) << '\n';
+		return EXIT_FAILURE;
+	}
+
+	// original
+	// write_value<<<1, 1>>>(device_output);
+	// work 1
+	write_value<<<1, 1>>>(device_output, VALUE);
+	// work 2 : 44 != VALUE
+	write_value<<<1, 1>>>(device_output, 44);
+
+	status = cudaGetLastError();
+	if (status == cudaSuccess) {
+		status = cudaDeviceSynchronize();
+	}
+	if (status == cudaSuccess) {
+		status = cudaMemcpy(&host_output, device_output, sizeof(int), cudaMemcpyDeviceToHost);
+	}
+
+	const cudaError_t free_status = cudaFree(device_output);
+
+	if (status != cudaSuccess || free_status != cudaSuccess) {
+		std::cerr << "cuda failed\n";
+	}
+
+	if (host_output != VALUE) {
+		std::cerr << "Unexpected result : " << host_output << '\n';
+		return EXIT_FAILURE;
+	}
+
+	std::cout << "cuda success: " << host_output << '\n';
+	return EXIT_SUCCESS;
+
+}
